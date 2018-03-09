1a609003afadc79f876695b4c11cbc59be6552fa
Tomasz Oponowicz <tomasz.oponowicz@zaxo.biz>
2018-03-09 20:17:51 +0100

    Group images into single features bundle

---
+++ features_bundle.py
@@ -0,0 +1,11 @@
+import dask.array as da
+
+
+def images_to_bundle(source, target, name):
+    da.image.imread(source + '*.jpg').to_hdf5(target, name)
+
+
+if __name__ == "__main__":
+    images_to_bundle('./build/valid/*.png', 'valid.h5', 'data')
+    # images_to_bundle('./build/test/*.png', 'test.h5', 'data')
+    # images_to_bundle('./build/train/*.png', 'train.h5', 'data')
