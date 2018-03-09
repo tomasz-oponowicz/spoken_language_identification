ff056bcf7ca1cd8394424017cc2b131e34353883
Tomasz Oponowicz <tomasz.oponowicz@zaxo.biz>
2018-03-09 22:39:50 +0100

    Convert test and train images too

--- features_bundle.py
+++ features_bundle.py
@@ -7,5 +7,5 @@ def images_to_bundle(source, target, name):
 
 if __name__ == "__main__":
     images_to_bundle('./build/valid/*.png', 'valid.h5', 'data')
-    # images_to_bundle('./build/test/*.png', 'test.h5', 'data')
-    # images_to_bundle('./build/train/*.png', 'train.h5', 'data')
+    images_to_bundle('./build/test/*.png', 'test.h5', 'data')
+    images_to_bundle('./build/train/*.png', 'train.h5', 'data')
