bee0a3d46110f92cca2205ea526a07a42d5fda11
Tomasz Oponowicz <tomasz.oponowicz@zaxo.biz>
2018-03-09 15:56:14 +0100

    Improve features generation

--- features.py
+++ features.py
@@ -1,34 +1,68 @@
 import librosa as lr
-import os
 import glob
 import imageio
 import os
+import librosa.display
+import matplotlib.pyplot as plt
+import time
 
 
-def mp3_to_img(path, height=192, width=192):
-    signal, sr = lr.load(path, res_type='kaiser_fast')
-    hl = signal.shape[0]//(width*1.1)  # this will cut away 5% from start and end
-    spec = lr.feature.melspectrogram(signal, n_mels=height, hop_length=int(hl))
-    img = lr.logamplitude(spec)**2
-    start = (img.shape[1] - width) // 2
-    return img[:, start:start+width]
+def audio_to_spectrogram(path, height=192, width=192):
+    signal, sample_rate = lr.load(path, res_type='kaiser_fast')
+
+    hop_length = signal.shape[0] // float(width)
+    spectrogram = lr.feature.melspectrogram(signal, n_mels=height, hop_length=int(hop_length))
+    log_spectrogram = lr.amplitude_to_db(spectrogram)
+
+    # trim
+    log_spectrogram = log_spectrogram[:, 0:width]
+
+    return log_spectrogram, sample_rate, hop_length
 
 
-def process_audio(input_dir):
+def process_audio(input_dir, debug=False):
     files = []
 
-    extensions = ('*.mp3', '*.ogg')
+    extensions = ['*.flac']
     for extension in extensions:
         files.extend(glob.glob(os.path.join(input_dir, extension)))
 
     for file in files:
-        image = mp3_to_img(file)
+        print(file)
+
+        start = time.time()
+
+        spectrogram, sample_rate, hop_length = audio_to_spectrogram(file)
 
         file_without_ext = os.path.splitext(file)[0]
-        imageio.imwrite(file_without_ext + '.jpg', image)
+
+        # use loseless image format, e.g. png
+        imageio.imwrite(file_without_ext + '.png', spectrogram, compress_level=6)
+
+        if debug:
+            end = time.time()
+            print("It took [s]: ", end - start)
+
+            # Make a new figure
+            plt.figure(figsize=(12, 4))
+
+            # Display the spectrogram on a mel scale
+            # sample rate and hop length parameters are used to render the time axis
+            lr.display.specshow(spectrogram, sr=sample_rate, hop_length=int(hop_length),
+                                x_axis='time', y_axis='mel')
+
+            # Put a descriptive title on the plot
+            plt.title('mel power spectrogram')
+
+            # draw a color bar
+            plt.colorbar(format='%+02.0f dB')
+
+            # Make the figure layout compact
+            plt.tight_layout()
+            plt.show()
 
 
 if __name__ == "__main__":
-    process_audio('test')
-    process_audio('train')
-    process_audio('valid')
+    process_audio('build/valid')
+    # process_audio('build/test')
+    # process_audio('build/train')
