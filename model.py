e27a862049bcb76938240614104ac76c9406f98f
Tomasz Oponowicz <tomasz@zaxo.biz>
2018-03-16 10:04:25 +0100

    Create load_data helper method

--- model.py
+++ model.py
@@ -14,35 +14,33 @@ in_dim = (192,192,1)
 out_dim = 3
 batch_size = 32
 
+def load_data(label_binarizer, file):
+    bundle = np.load(file)
+
+    metadata = bundle['labels']
+
+    labels = label_binarizer.transform(metadata[:, 0])
+
+    features = bundle['features']
+    features = features.reshape((len(features), 192, 192, 1))
+    features = np.divide(features, 255.)
+
+    print("[{file}] labels: {labels}, features: {features} (max: {max}, min: {min})".format(
+        file=file, labels=labels.shape, features=features.shape, 
+        max=np.max(features), min=np.min(features)
+    ))
+
+    return (labels, features, metadata)
+
+
 def create_model():
-    lb = preprocessing.LabelBinarizer()
-    lb.fit(['en', 'de', 'es'])
-    print(lb.classes_)
+    label_binarizer = preprocessing.LabelBinarizer()
+    label_binarizer.fit(['en', 'de', 'es'])
+    print(label_binarizer.classes_)
 
-    train = np.load('train.npz')
-    valid = np.load('valid.npz')
-    test = np.load('test.npz')
-
-    train_metadata = train['labels']
-    train_labels = lb.transform(train_metadata[:, 0])
-    train_features = train['features']
-    train_features = train_features.reshape((len(train_features), 192, 192, 1))
-    train_features = np.divide(train_features, 255.)
-    print(train_labels.shape, train_features.shape, np.max(train_features), np.min(train_features))
-
-    valid_metadata = valid['labels']
-    valid_labels = lb.transform(valid_metadata[:, 0])
-    valid_features = valid['features']
-    valid_features = valid_features.reshape((len(valid_features), 192, 192, 1))
-    valid_features = np.divide(valid_features, 255.)
-    print(valid_labels.shape, valid_features.shape, np.max(valid_features), np.min(valid_features))
-
-    test_metadata = test['labels']
-    test_labels = lb.transform(test_metadata[:, 0])
-    test_features = test['features']
-    test_features = test_features.reshape((len(test_features), 192, 192, 1))
-    test_features = np.divide(test_features, 255.)
-    print(test_labels.shape, test_features.shape, np.max(test_features), np.min(test_features))
+    train_labels, train_features, train_metadata = load_data(label_binarizer, 'train.npz')
+    valid_labels, valid_features, valid_metadata = load_data(label_binarizer, 'valid.npz')
+    test_labels, test_features, test_metadata = load_data(label_binarizer, 'test.npz')
 
     i = Input(shape=in_dim)
     m = Conv2D(16, (3, 3), activation='elu', padding='same')(i)
@@ -62,7 +60,7 @@ def create_model():
 
     model = Model(inputs=i, outputs=o)
     model.summary()
-    
+
     model.compile(loss='categorical_crossentropy', optimizer=Nadam(lr=1e-4), metrics=['accuracy'])
     model.fit(train_features, train_labels, epochs=10, verbose=1, validation_data=(valid_features, valid_labels))
 
