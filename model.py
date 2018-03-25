045761a226a78cc2c4f09246e5fa878621361a0f
Tomasz Oponowicz <tomasz@zaxo.biz>
2018-03-25 01:10:59 +0100

    F1: 87/72 (all)

--- model.py
+++ model.py
@@ -193,12 +193,12 @@ def train_model(train_labels, train_features, valid_labels, valid_features,
     model.add(Flatten())
 
     model.add(Dense(64))
-    model.add(BatchNormalization())
     model.add(Activation('elu'))
+    model.add(BatchNormalization())
 
     model.add(Dense(32))
-    model.add(BatchNormalization())
     model.add(Activation('elu'))
+    model.add(BatchNormalization())
 
     model.add(Dropout(0.5))
 
