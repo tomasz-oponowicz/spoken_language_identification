161ed6c1f1a6155da6f2be232b9a996ac7b12906
Tomasz Oponowicz <tomasz@zaxo.biz>
2018-04-06 20:23:46 +0200

    F1: 81

--- mfcc_model.py
+++ mfcc_model.py
@@ -59,16 +59,16 @@ def build_model(input_shape):
 
     # 12x1000
 
-    model.add(Conv2D(16, (3, 3), strides=(1, 1), padding='same', input_shape=input_shape))
+    model.add(Conv2D(8, (3, 5), strides=(1, 1), padding='same', input_shape=input_shape))
+    model.add(Conv2D(8, (3, 5), strides=(1, 1), padding='same'))
+    model.add(Conv2D(8, (3, 5), strides=(1, 1), padding='same'))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(3,3), strides=(1,2), padding='same'))
 
     # 12x500
 
     model.add(Conv2D(8, (3, 5), strides=(1, 1), padding='same'))
-    # model.add(Activation('relu'))
     model.add(Conv2D(8, (3, 5), strides=(1, 1), padding='same'))
-    # model.add(Activation('relu'))
     model.add(Conv2D(8, (3, 5), strides=(1, 1), padding='same'))
     model.add(Activation('relu'))
 
